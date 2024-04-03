b0238886a3f97331123c23f814176522e3b8336e
Phong Nguyen <phrongnguyn@gmail.com>
2024-04-03 00:49:41 -0400

    doing the page

---
+++ src/java/businesslayer/UserBusinessLogic.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package businesslayer;
+
+import dataaccesslayer.UserDAOImpl;
+import java.sql.SQLException;
+import java.util.List;
+import transferobject.UserDTO;
+
+/**
+ *
+ * @author phron
+ */
+public class UserBusinessLogic {
+     private UserDAOImpl usersDao = null;
+
+    public UserBusinessLogic() {
+        usersDao = new UserDAOImpl();
+    }
+
+    public List<UserDTO> getAllUsers() throws SQLException {
+        return usersDao.getAllUsers();
+    }
+
+    public void addUser(UserDTO user) {
+        usersDao.addUser(user);
+    }
+    public boolean validateCredentials(String username, String password) {
+        return usersDao.validate(username, password);
+    }
+}
