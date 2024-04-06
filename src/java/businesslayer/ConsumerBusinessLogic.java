e67303fd4f12659aed7b720f85b48ce33238b6c6
Wenqi Tang <tang0246@algonquinlive.com>
2024-04-06 15:36:46 -0400

    ConsumerBusinessLogic and ConsumerServlet

---
+++ src/java/businesslayer/ConsumerBusinessLogic.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package businesslayer;
+
+import dataaccesslayer.FoodItemDAO;
+import dataaccesslayer.TransactionsDAO;
+
+
+/**
+ *
+ * @author Shao Tang
+ */
+public class ConsumerBusinessLogic {
+    private TransactionsDAO transactionsDAO;
+    private FoodItemDAO itemDAO;
+  
+    public ConsumerBusinessLogic(){
+    }
+    public ConsumerBusinessLogic(TransactionsDAO transactionsDAO, FoodItemDAO itemDAO) {
+        this.transactionsDAO = transactionsDAO;
+        this.itemDAO = itemDAO;
+    }
+    
+   public void purchaseItem(int userId, int itemId, int quantity) throws Exception {
+        
+        boolean isAvailable = checkInventoryAvailability(itemId, quantity);
+        if (!isAvailable) {
+            throw new Exception("Item not available in sufficient quantity.");
+        }
+
+        
+        TransactionDTO transaction = new TransactionDTO();
+        transaction.setUserId(userId);
+        transaction.setItemId(itemId);
+        transaction.setTransactionType("PURCHASE");
+        transaction.setTransactionDate(new java.sql.Timestamp(new java.util.Date().getTime())); 
+        transactionsDAO.addTransaction(transaction);
+
+       
+        itemDAO.updateFoodItem(itemId, -quantity); 
+    }
+
+    private boolean checkInventoryAvailability(int itemId, int quantity) {
+        //
+        return true;
+    }
+}
