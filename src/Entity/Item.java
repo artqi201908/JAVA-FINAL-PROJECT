2b62900ffc37c32f00709d612974d36049ba7c61
Wenqi Tang <tang0246@algonquinlive.com>
2024-04-06 20:52:47 -0400

    ConsumerBusinessLogic and ConsumerServlet

---
+++ src/Entity/Item.java
@@ -0,0 +1,98 @@
+
+import java.util.Date;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Shao Tang
+ */
+public class Item {
+    private int itemId;
+    private String name;
+    private int quantity;
+    private Date expirationDate;
+
+
+
+    private double price;
+    private double discountRate;
+    private boolean isForDonation;
+    private int userId;
+
+
+    public Item() {
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
+
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+
+    public boolean isForDonation() {
+        return isForDonation;
+    }
+
+    public void setForDonation(boolean forDonation) {
+        isForDonation = forDonation;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+}
