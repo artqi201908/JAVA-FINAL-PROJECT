ee7d10bf5042b4f216d16ab5cbc31552fec33d09
Danni Hu <danniyh@outlook.com>
2024-04-09 17:35:47 -0400

    modify package name, draft

--- src/java/transferobject/UserValidationResult.java
+++ src/java/transferobject/UserValidationResult.java
@@ -1,80 +1,49 @@
-<<<<<<< HEAD
+
+
 package java.transferobject;
 
 import java.util.Date;
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package transferobject;
->>>>>>> origin/main
 
 /**
  *
  * @author phron
  */
-public class UserValidationResult {
+public abstract class UserValidationResult {
 
-<<<<<<< HEAD
     private Long createUserId;
     private Date createDate = new Date();
     private Long modUserId;
     private Date modDate;
 
     public Long getCreateUserId() {
-        return createUserId;
+        return this.createUserId;
     }
 
     public void setCreateUserId(Long createUserId) {
         this.createUserId = createUserId;
     }
 
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
     public Long getModUserId() {
-        return modUserId;
+        return this.modUserId;
     }
 
     public void setModUserId(Long modUserId) {
         this.modUserId = modUserId;
     }
 
+    public Date getCreateDate() {
+        return this.createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
     public Date getModDate() {
-        return modDate;
+        return this.modDate;
     }
 
     public void setModDate(Date modDate) {
         this.modDate = modDate;
     }
-
-
-
-
-
-
-=======
-    private Integer userId;
-    private Integer userType;
-
-    public UserValidationResult(Integer userId, Integer userType) {
-        this.userId = userId;
-        this.userType = userType;
-    }
-
-    // Getters
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public Integer getUserType() {
-        return userType;
-    }
->>>>>>> origin/main
 }
