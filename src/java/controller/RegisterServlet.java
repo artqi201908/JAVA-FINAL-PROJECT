97c21fda929219fd8396774199598ee66bb1756f
Danni Hu <danniyh@outlook.com>
2024-04-10 12:11:18 -0400

    try to modify confilict

--- src/java/controller/RegisterServlet.java
+++ src/java/controller/RegisterServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.businesslayer.UserBusinessLogic;
 import java.businesslayer.ValidateException;
 import java.io.IOException;
 import java.math.BigDecimal;
