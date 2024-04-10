93baf16459cfdb7bd1448dfaf97776cf37f007ea
Danni Hu <danniyh@outlook.com>
2024-04-10 17:38:30 -0400

    try to modify confilict

--- src/java/constant/UserType.java
+++
@@ -1,7 +0,0 @@
-package java.constant;
-
-public interface UserType {
-    long RETAILER = 1L;
-    long CHARITABLE_ORGANIZATION = 2L;
-    long CONSUMER = 3L;
-}
--- src/java/constant/orderStatus.java
+++
@@ -1,6 +0,0 @@
-package java.constant;
-
-public interface orderStatus {
-    long PENDING_APPROVE = 1L;
-    long APPROVED = 2L;
-}
