d4cd67b1b8c588eb36f84e64c29c3496a4b1a7b0
Danni Hu <danniyh@outlook.com>
2024-04-09 23:02:30 -0400

    modify stucture of navibar

---
+++ src/web/Navbar.jsp
@@ -0,0 +1,16 @@
+
+<div class="top-nav-bar">
+
+
+
+<div class="logo">
+    <h2>FWRP</h2>
+</div>
+
+
+<div class="profile">
+
+</div>
+
+
+</div>
--- src/web/main.js
+++ src/web/main.js
@@ -3,11 +3,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
 
+<%@page import="java.util.List" %>
 
 
 
-function buyItem(itemId) {
-    // need to code here
+<%
+    User user = (User) session.getAttribute("user");
+%>
 
-    alert("Buying Item with ID: " + itemId);
-}
+
+<% if (user != null && user.getTypeId() == UserType.RETAILER) { %>
+
+<div class="content">
+        <table border="1">
+        <thead>
+        <tr>
+        <th>id</th>
+    <th>title</th>
+    <th>quantity</th>
+    <th>expirationDate</th>
+    <th>isDonate</th>
+    <th>price</th>
+    <th>disCount</th>
+    <th>isSurplus</th>
+    <th>Action</th>
+</tr>
+</thead>
+    <tbody>
+    <% List<Item> items = (List<Item>) request.getAttribute("items");
+        for (Item item : items) {%>
+            <tr>
+            <td><%= item.getId()%></td>
+            <td><%= item.getTitle()%></td>
+            <td><%= item.getQuantity()%></td>
+            <td <% if(DateUtil.isExpiredInNextWeek(item.getExpirationDate())) { out.print("style='color: red;'"); } %>>
+            <%= item.getExpirationDate()%>
+            </td>
+            <td><%= item.getIsDonate()%></td>
+            <td><%= item.getPrice()%></td>
+            <td><%= item.getDiscount()%></td>
+            <td><%= item.getIsSurplus()%></td>
+            <td>
+            <a href="updateItem?itemId=<%=item.getId()%>">Edit</a>
+            <a href="deleteItem?itemId=<%=item.getId()%>">Delete</a>
+            </td>
+            </tr>
+            <% }%>
+    </tbody>
+</table>
+    <div>
+        <%
+        if (items == null || items.size() == 0) {
+        out.println("No results.");
+    }
+        %>
+    </div>
+</div>
+</div>
+    <% }%>
