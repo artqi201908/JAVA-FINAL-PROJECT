ab72d455badf6b35f587f057817e09563dfc4a27
Danni Hu <danniyh@outlook.com>
2024-04-10 12:14:18 -0400

    try to modify confilict

--- src/java/businesslayer/UserBusinessLogic.java
+++ src/java/businesslayer/UserBusinessLogic.java
@@ -1,6 +1,34 @@
 package java.businesslayer;
 
+import javax.xml.registry.infomodel.User;
+import java.util.List;
+
 public class UserBusinessLogic {
+    public UserBusinessLogic() {
+    }
 
+    public User findByUsername(String username) {
+        return null;
+    }
 
+    public User findByEmail(String email) {
+        return null;
+    }
+
+    public void create(User user) throws ValidateException.ValidationException {
+    }
+
+    public void update(User user) throws ValidateException.ValidationException {
+    }
+
+    private void validateUser(User user) throws ValidateException.ValidationException {
+    }
+
+    public User findById(Long userId) {
+        return null;
+    }
+
+    public List<User> findSubscribedUsers() {
+        return null;
+    }
 }
